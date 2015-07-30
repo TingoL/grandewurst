8155114ea9af503cb5bb7751f30e3011e829db56
tingol <amelxmx@gmail.com>
2015-07-30 17:42:52 +0200

    meni skeletor

--- meni.php
+++ meni.php
@@ -3,5 +3,84 @@
     require("header.php");
    ?>  
 
+  <div class="sekcija-1">
+    <div class="container">
+      <div class="row">
+        <div class="one-half column slika">
+          <img class="meni-kobaje" src="public/images/lel.jpg" />
+        </div>
+    <div class="container">
+      <div class="row">
+        <div class ="one-half column tabela">
+          <table class="u-full-width">
+            <tbody>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+            <tbody>
+           </table>
+          </div> 
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sekcija-2">
+    <div class="container">
+      <div class="row">
+        <div class="one-half column slika">
+          <img class="meni-kobaje" src="public/images/lel.jpg" />
+        </div>
+    <div class="container">
+      <div class="row">
+        <div class ="one-half column tabela">
+          <table class="u-full-width">
+            <tbody>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+                <tr>
+                  <td>Kobaja</td>
+                  <td>100rsd</td>
+                </tr>
+            <tbody>
+          </table>
+        </div>
+      </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <?php require("footer.php"); ?>
