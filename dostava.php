21a3f6871d424a255e068b0ea2c427b1e13b0d0b
Pekmen <milanpekovic89@gmail.com>
2015-08-14 01:47:05 +0200

    poboljsana dostava

--- dostava.php
+++ dostava.php
@@ -8,26 +8,45 @@
     <div id="dostava">
       <div id="gmap">
       </div>  
-	</div><!-- end dostava -->
+	  </div><!-- end dostava -->
 
-    <div class="dostava-naslov">
-      <h2>Lorem ipsum dolor sit amet, consectetur!</h2>
-    </div>
-      
+    <div class="kontakt-wrapper">
+      <div class="container">
+        <div class="kontakt">
+
+          <div class="five columns">
+            <h3>Adresa</h3>
+              <p>Bulevar Zorana Đinđića 12g<br>11070 Beograd - Novi Beograd</p>
+              <p>Bulevar Zorana Đinđića 12g<br>11070 Beograd - Novi Beograd</p>
+          </div>
+
+          <div class="three columns">
+            <h3>Telefon</h3>
+            <p>060/ 123 4567<br>060/ 123 4567</p>
+           </div>
+          
+          <div class="three columns">
+            <h3>Email</h3>
+            <p>grandewurst@hotmail.rs</p>
+          </div>
+        </div><!-- end kontakt -->
+      </div>
+    </div><!-- end kontakt-wrapper -->
+    
+    <div class="container">
+      <div class="dostava-naslov">
+        <h2>Lorem ipsum dolor sit amet, consectetur!</h2>
+        <p class="border"></p>
+      </div>
       <div class="dostava-description">
-        <div class="container">
-          <div class="row">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et urna varius, finibus lorem porttitor, semper lacus.
-                Morbi elementum ultricies ultricies. Sed vitae urna sed enim gravida venenatis eget ac ex. Mauris non erat neque.
-                Morbi tempor urna nec metus eleifend ornare. Morbi sed euismod lorem, vitae iaculis enim. 
-                Praesent fermentum et augue non pulvinar. Nunc interdum, libero at efficitur iaculis, tellus quam consequat magna, 
-                sed sodales ipsum urna vel nunc. Etiam et dictum enim.
-            </p>
-        </div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id odio egestas, porta ante vel, porttitor risus.
+        Nunc venenatis lectus ut pellentesque maximus. Integer eu fermentum lacus. Quisque a maximus dui.
+        Donec malesuada feugiat libero, vitae mattis est sollicitudin in. 
+        Nunc pharetra eros vel convallis mollis. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Sed faucibus urna turpis, sed facilisis tortor molestie id. In molestie orci a venenatis rhoncus.
       </div>
     </div>
-
     
   </div><!-- end dostava-wrapper -->
 
